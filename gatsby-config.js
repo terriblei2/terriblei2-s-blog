92fa9bb7f9d57fd50eb055f8c7ec4438776d0e0b
terriblei2 <terriblei2ii@gmail.com>
2021-11-04 18:48:31 +0900

    feat: pluginの追加,  型定義ファイルの追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,6 +72,14 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /src/images, // 読み込みたいSVGファイルが置いてあるフォルダを指定する
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
